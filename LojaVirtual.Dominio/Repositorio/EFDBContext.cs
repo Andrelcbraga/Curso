7023162f41bb57389a180dd968adfdad3f4f55e6
Foxanger <andreluizcb2@gmail.com>
2015-10-28 18:02:22 -0300

    Quarta feira 28/10 as 18

---
+++ LojaVirtual.Dominio/Repositorio/EFDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using LojaVirtual.Dominio.Entidades;
+
+
+namespace LojaVirtual.Dominio.Repositorio
+{
+  public class EFDBContext: DbContext
+    {
+      public DbSet<Produto> Produtos { get; set; }
+    }
+}
